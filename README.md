ea502f2cb9df5691a4b169743d2bb429bef119d6
cdp50 <par17069@byui.edu>
2022-12-12 16:50:34 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Overview
+
+I build my first Kotlin app. My main desire here was to learn how to the Kotlin language works. I was curious how different it would be from building just a webapp. So I build a simple checklist app. An app that takes input from the user and place it right besides a checkbox. You can check the box, uncheck it, and delete the marked boxes from the list. 
+
+[To-do list Kotlin app Demo Video](https://youtu.be/z6VBQcOMu-s)
+
+# Development Environment
+
+In order to accomplish this task I downloaded and learned to use Android Studio. And I also had to learn how to use Recycler View a library that allows to reuse the views and populate them with content. All of this done of course in Kotlin (that was the challenge)
+
+# Useful Websites
+
+* [Developers.android.com](https://developer.android.com/training/basics/firstapp) - Create a Hello World app
+* [Developers.android.com](https://developer.android.com/guide) - Guides and Documentation 
+* [Youtube](https://www.youtube.com/watch?v=Mc0XT58A1Z4) -      Recycler view
+* [YouTube](https://www.youtube.com/watch?v=F9UC9DY-vIU&t=2860s) - Kotlin course
+
+
+# Future Work
+
+* Add gestures, I want to be able to delete the items of the list with a swipe gesture
+* Learn how to take more advantage of the .xml files to style my apps
+* Implement a database. 
---
+++ app/src/main/java/com/example/todolist/TodoAdapter.kt
@@ -0,0 +1,64 @@
+package com.example.todolist
+
+import android.graphics.Paint.STRIKE_THRU_TEXT_FLAG
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.item_todo.view.*
+// the list that will hold the layout of a specific item
+class TodoAdapter(
+    private val todos: MutableList<Todo>
+) : RecyclerView.Adapter<TodoAdapter.TodoViewHolder>() {
+
+    class TodoViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoViewHolder {
+        return TodoViewHolder(
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.item_todo,
+                parent,
+                false
+            )
+        )
+    }
+
+    fun addTodo(todo: Todo) {
+        todos.add(todo)
+        notifyItemInserted(todos.size - 1)
+    }
+
+    // it will go through our todos list and give us an instance of each todo Item
+    fun deleteDoneTodos() {
+        todos.removeAll { todo ->
+            todo.isChecked
+        }
+        notifyDataSetChanged()
+    }
+
+    private fun toggleStrikeThrough(tvTodoTitle: TextView, isChecked: Boolean) {
+        if(isChecked) {
+            tvTodoTitle.paintFlags = tvTodoTitle.paintFlags or STRIKE_THRU_TEXT_FLAG
+        } else {
+            tvTodoTitle.paintFlags = tvTodoTitle.paintFlags and STRIKE_THRU_TEXT_FLAG.inv()
+        }
+    }
+    // It will bind the data form our todos list to the view of our list
+    override fun onBindViewHolder(holder: TodoViewHolder, position: Int) {
+        val curTodo = todos[position]
+        holder.itemView.apply {
+            tvTodoTitle.text = curTodo.title
+            cbDone.isChecked = curTodo.isChecked
+            toggleStrikeThrough(tvTodoTitle, curTodo.isChecked)
+            cbDone.setOnCheckedChangeListener { _, isChecked ->
+                toggleStrikeThrough(tvTodoTitle, isChecked)
+                curTodo.isChecked = !curTodo.isChecked
+            }
+        }
+    }
+    // It returns the amount of items that we have on our list.
+    override fun getItemCount(): Int {
+        return todos.size
+    }
+}
